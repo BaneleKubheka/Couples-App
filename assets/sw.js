bd8aefa6f170eb86555862170e98a87aa7fede10
BaneleKubheka <banelekubheka45.1@gmail.com>
2026-06-17 14:27:43 +0200

    Add files via upload

---
+++ assets/sw.js
@@ -0,0 +1,17 @@
+const CACHE = 'couples-connect-clean-no-version-errors-20260617';
+const ASSETS = ['./', './index.html', './styles.css', './app.js', './manifest.json', './icon.png'];
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim()));
+});
+self.addEventListener('fetch', event => {
+  const request = event.request;
+  if (request.method !== 'GET') return;
+  event.respondWith(fetch(request).then(response => {
+    const copy = response.clone();
+    caches.open(CACHE).then(cache => cache.put(request, copy)).catch(() => {});
+    return response;
+  }).catch(() => caches.match(request)));
+});
