1c76e130b3c0a802f166cde013dded03a047b74e
BaneleKubheka <banelekubheka45.1@gmail.com>
2026-06-17 13:02:01 +0200

    Add files via upload

--- assets/sw.js
+++ assets/sw.js
@@ -1,4 +1,4 @@
-const CACHE='couples-connect-cache-bypass-signin-20260617-15';
+const CACHE='couples-connect-admin-location-16';
 const ASSETS=['./','./index.html','./styles.css','./app-v15.js','./app.js','./manifest.json','./icon.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
